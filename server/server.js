eab3b34f3ed8514fd82361505bc617aaeeb8edc2
gel0king <92491316+gel0king@users.noreply.github.com>
2024-10-19 19:09:39 -0500

    added server responses

--- server/server.js
+++ server/server.js
@@ -11,10 +11,11 @@ PORT=8080;
 let db;
 (async () => {
 	db = await open({
-		filename: 'data.sqlite',
+		filename: 'users.db',
 		driver: sqlite3.Database
 	});
 })();
+
 
 app = express();
 app.use(express.static(path.join(__dirname, 'static')));
@@ -25,24 +26,61 @@ app.get('/', (req,res) =>{
 	res.json({});
 });
 
-app.get('/friends', async (req,res) =>{
-	const result = await db.all('SELECT * from friend');
+app.get('/test', async (req,res) =>{
+	res.json("Hello world");
+});
+
+//get data from all users
+app.get('/users', async (req,res)=>{
+	const result = await db.all('SELECT * from user');
 	res.json(result);
-});
+})
 
-app.get('/courses', async (req,res) => {
-	const courses = await db.all('SELECT * from course');
-	const sections = await db.all('SELECT * from section');
-	const instructor = await db.all('SELECT * from instructor');
-	courses.map(course => course["sections"] = sections.filter(section => section.course_id == course.id));
-	courses.map(course => console.log(course));
-	res.json({instructor, courses});
-});
+//get all events
+app.get('/events', async (req,res)=>{
+	const result = await db.all('SELECT * from event');
+	res.json(result);
+})
 
-app.post('/add', async(req,res) => {
-	let courseId = req.body.courseId;
-	let instructor = req.body.instructor;
-	const result = await db.run("INSERT INTO section (course_id, insturctor_id) VALUES (?,?)", [courseId, instructor]);
+//add new user
+app.post('/add_user', async(req,res) => {
+	let name = req.body.name;
+	let email = req.body.email;
+	let password = req.body.password;
+	let host = req.body.host;
+	const result = await db.run("INSERT INTO user (Name, Email, Password,Host) VALUES (?,?,?,?)", [name, email, password, host]);
+	res.json(result);
+})
+
+//add new event
+app.post('/add_event', async(req,res) => {
+	let host_id = req.body.host_id;
+	let host = req.body.host;
+	let location = req.body.location;
+	let date = req.body.date;
+	const result = await db.run("INSERT INTO event (Host_ID, Host, Location, Date) VALUES (?,?,?,?)", [host_id, host, location, date]);
+	res.json(result);
+})
+
+//login
+app.get('/login', async(req,res) => {
+	let email = req.body.email;
+	let password = req.body.password;
+	const result = password == db.run("SELECT Password FROM user WHERE Email = (?)",[email]);
+	res.json(result);
+})
+
+//view event by location
+app.get('/add_event', async(req,res) => {
+	let email = req.body.location;
+	const result = db.run("SELECT * FROM event WHERE location = (?)",[location]);
+	res.json(result);
+})
+
+//view event by date
+app.get('/add_event', async(req,res) => {
+	let email = req.body.date;
+	const result = db.run("SELECT * FROM event WHERE date = (?)",[date]);
 	res.json(result);
 })
 
